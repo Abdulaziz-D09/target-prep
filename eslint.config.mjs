258c98eb4b46138d93d625d00a2e80861e0322fa
Abdulaziz Davronov <abdulazizdavronov@Macbook-Pro-M3-Pro.local>
2026-06-15 10:17:56 +0500

    work?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,12 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "src/**",
+    "*.ts",
+    "*.tsx",
+    "*.js",
+    "pdf_extractor/**",
+    "scripts/**"
   ]),
 ]);
 
